c1b5a1facddc8e8ba0c88b73a8c43c102597bb48
Jurvis Tan <jurvis.tan@me.com>
2014-07-02 11:16:36 +0800

    half hour scrape

--- src/github.com/jurvis/model/model.go
+++ src/github.com/jurvis/model/model.go
@@ -28,7 +28,7 @@ func StoreData() {
 	s.Flush()
 
 	// set up a goroutine to scrape every 1 Hour
-	ticker := time.NewTicker(1 * time.Hour)
+	ticker := time.NewTicker(30 * time.Minute)
 	quit := make(chan struct{})
 	go func() {
 		for {
