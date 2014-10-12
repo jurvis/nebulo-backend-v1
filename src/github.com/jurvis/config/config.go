7c61fbc84d74c684b2861deca3a2aba4c18d04a0
Jurvis Tan <jurvis.tan@me.com>
2014-10-12 12:45:35 +0800

    some thing that does that push thing

---
+++ src/github.com/jurvis/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"code.google.com/p/gcfg"
+)
+
+type DbCfg struct {
+	Database struct {
+		Username string
+		Password string
+		Dbname   string
+		Host     string
+	}
+}
+
+type TwCfg struct {
+	Application struct {
+		ApiKey    string
+		ApiSecret string
+	}
+	Consumer struct {
+		Token  string
+		Secret string
+	}
+}
+
+// Read .gcfg file into struct
+func DbConfig() DbCfg {
+	var cfg DbCfg
+	gcfg.ReadFileInto(&cfg, "dbconfig.gcfg")
+	return cfg
+}
+
+func TwitterConfig() TwCfg {
+	var cfg TwCfg
+	gcfg.ReadFileInto(&cfg, "twitterconfig.gcfg")
+}
--- src/github.com/jurvis/db/db.go
+++ src/github.com/jurvis/db/db.go
@@ -6,19 +6,14 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	DB_USERNAME = "postgres"
-	DB_PASSWORD = "postgres"
-	DB_NAME     = "nebulo"
-	DB_HOST     = "localhost"
-)
-
 func Dbconnect() (*sql.DB, error) {
+	cfg := config.Config()
+
 	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable",
-		DB_USERNAME,
-		DB_NAME,
-		DB_PASSWORD,
-		DB_HOST))
+		cfg.Database.Username,
+		cfg.Database.Dbname,
+		cfg.Database.Password,
+		cfg.Database.Host))
 
 	return db, err
 }
